30f2c444c926e13c0470fafe8995ec1e16a4261b
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-01-04 21:19:47 +0330

    create project noteApp

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Note from "./components/noteApp/Note.jsx"
+const App=()=> {
+    return (
+    <>
+        <Note name={"amir"}/>
+    </>
+  )
+}
+export default App
---
+++ src/components/noteApp/Note.jsx
@@ -0,0 +1,39 @@
+import {useState} from "react";
+const App=(props)=> {
+    const [notes,setNotes]=useState(["rice","amir","mamad"])
+    const [giving,setgiving]=useState()
+    const [show,setShow]=useState(false)
+    const give=(e)=>{
+        setgiving(e.target.value)
+    }
+    const add=()=>{
+        let myState=[...notes]
+        myState.push(giving)
+        setNotes(myState)
+    }
+    const toggle=()=>{
+        const showOrHide=show
+        setShow(!showOrHide)
+    }
+    const removeNote=(nI)=>{
+        let newNote=[...notes]
+        newNote.splice(nI,1)
+        setNotes(newNote)
+    }
+    return (
+        <>
+            <button onClick={toggle}>show/hide</button>
+            {show ? (
+                <div>
+                    <input type="text" value={giving} onInput={(e) => give(e)}/>
+                    <h1>{props.name}</h1>
+                    <ul>
+                        {notes.map((note,noteIndex)=><li onClick={()=>removeNote(noteIndex)}>{note}</li>)}
+                    </ul>
+                    <button onClick={add}>add</button>
+                </div>
+            ) : null }
+        </>
+    )
+}
+export default App
